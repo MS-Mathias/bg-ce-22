b4820b3092e13589343cd596ffb123a027ef5365
MS-Mathias <mathias.varela@ms-argentina.com>
2022-05-05 17:24:59 -0300

    Breves cambios en el codigo
    El codigo sigue siendo escencialmente lo mismo, lo unico que cambia es que borre algunas lineas innecesarias y agregue una semilla al principio de la simulacion para replicabilidad del calculo (esto despues se tiene que borrar)

--- SimulacionesInicialesTasa.py
+++ SimulacionesInicialesTasa.py
@@ -1,22 +1,21 @@
 # Importo librerias 
 import pandas as pd
 import numpy as np
-import matplotlib as plt
 
 # Importo tabla (por ahora como no tengo la tabla original estoy generando una aleatoria :P)
 # serieTasaBadlar = pd.read_csv("C:/Users/mathias.ezequiel.va1/")
-serieTasaBadlar = [1.1]
+serieTasaBadlar = [0.1]
 for i in range(359):
-    serieTasaBadlar.append(serieTasaBadlar[i]+np.random.normal(0,0.005))
+    serieTasaBadlar.append(serieTasaBadlar[i]+np.random.normal(0,0.01))
 
 # Extraigo la utlima tasa, la media de las tasas y el desvio estandar
 tasaBadlar = serieTasaBadlar[-1]
 stdBadlar = np.std(serieTasaBadlar)
 
-
+np.random.seed(1)
 tasaFutura = [tasaBadlar]
 periodos = [0]
-for i in range(3):
+for i in range(120):
     periodos.append(periodos[i] + 30)
     shock = np.random.lognormal()
     tasaFutura.append(tasaFutura[i] * np.e ** ((-0.5 * (stdBadlar ** 2)) + stdBadlar * shock))
@@ -25,11 +24,8 @@ df = pd.DataFrame(tasaFutura, index=periodos, columns=["Tasa"])
 df
 
 df["Tasa"].plot(title="Tasas Simuladas",
-                xticks=periodos,
-                yticks=[1,1.2,1.4,1.6,1.8,2],
                 xlabel="Dias",
                 ylabel="Tasa",
-                fontsize=9,
-                )
+                fontsize=9)
 
 help(df.plot)
