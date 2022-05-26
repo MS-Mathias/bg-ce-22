b0abde28375cca458a8dc3ab21272554405b38b7
MS-Mathias <mathias.varela@ms-argentina.com>
2022-05-26 14:38:48 -0300

    Compactación del codigo
    Previamente se realizaban dos loops en el codigo principal, ahora esos loops estan dendro de una función que automaticamente genera todas las transformaciones de tasas necesarias.

--- TasasSimuladasAnuales a TasasProgresivasDiarias.py
+++ TasasSimuladasAnuales a TasasProgresivasDiarias.py
@@ -17,18 +17,22 @@ def AcumulaTasas(Sim):
         SimTasasAcumuladas.append(((1+SimTasasAcumuladas[i-1]) * (1+Sim[i]))-1)
     return SimTasasAcumuladas
 
+def TratamientoTasas(TasasSim):
+    TasasDiarias = []
+    for Simulacion in TasasSim:
+        TasasDiarias.append(AnualADiario(Simulacion))
+    del Simulacion
+    TasasDiarias = np.array(TasasDiarias)
+    
+    TasasAcum = []
+    for Simulacion in TasasDiarias:
+        TasasAcum.append(AcumulaTasas(Simulacion))
+    del Simulacion
+    TasasAcum = np.array(TasasAcum)
+    return TasasAcum
+
 TasasSimuladas = pd.read_excel("C:/Users/mathias.ezequiel.va1/Desktop/TasasSimuladas.xlsx")
 TasasSim = np.transpose(TasasSimuladas.values)
 del TasasSimuladas
 
-TasasDiarias = []
-for Simulacion in TasasSim:
-    TasasDiarias.append(AnualADiario(Simulacion))
-del Simulacion
-TasasDiarias = np.array(TasasDiarias)
-
-TasasAcum = []
-for Simulacion in TasasDiarias:
-    TasasAcum.append(AcumulaTasas(Simulacion))
-del Simulacion
-TasasAcum = np.array(TasasAcum)
+Tasas = TratamientoTasas(TasasSim)
