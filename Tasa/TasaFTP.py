bc774a57a6e9bdfd41fa0e61488b871adc0d2ca4
MS-Mathias <mathias.varela@ms-argentina.com>
2022-06-01 15:33:52 -0300

    Ajustes a TasaFTP
    Agregue algunas correcciones y grafico

--- Tasa/TasaFTP.py
+++ Tasa/TasaFTP.py
@@ -1,5 +1,11 @@
+#%% Import de librerias relevantes
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
+from scipy.interpolate import make_interp_spline
+#%% Funcion flip string
+def flipArray(array):
+    return array[::-1]
 #%% Funcion que genera df de diferencia de tasas para todos los nodos
 def DiferenciaNodos(df):
     df1 = df.copy()
@@ -36,8 +42,19 @@ def simulacionCurva(dfDiferencia, M = 1000):
         shockIndependiente = np.array(ShockIndependiente(dfDiferencia))
         shockCorr = np.array(shockCorrelacionado(shockIndependiente, cholesky))
         curvaSimulada = np.add(ultimaCurva,shockCorr)
-        arraySimulaciones[i] = (curvaSimulada)
+        arraySimulaciones[i] = flipArray(curvaSimulada)
     return arraySimulaciones
+#%% Funcion que transforma las simulaciones para graficar smooth lines
+def smoothLine(arraySimulaciones):
+    smoothArray = np.zeros((len(arraySimulaciones),500))
+    t = np.arange(12)*30+30
+    Xline = np.linspace(t.min(), t.max(), 500)
+    for i in range(len(arraySimulaciones)):
+        XY_Spline = make_interp_spline(t, arraySimulaciones[i])
+        Yline = XY_Spline(Xline)
+        smoothArray[i] = Yline
+    smoothArray = smoothArray.transpose()
+    return Xline, smoothArray
 #%% Input del modelo
 dfInput = pd.read_excel("C:/Users/mathias.ezequiel.va1/Desktop/Banco Galicia - Capital Economico/Fase2/Tasa/InputsFalsos/TasaFTP.xlsx")
 ultimaCurva = dfInput.iloc[-1,1:].values
@@ -45,8 +62,15 @@ ultimaCurva = dfInput.iloc[-1,1:].values
 dfDiferencia = DiferenciaNodos(dfInput)
 #%% Genera la matriz covarianza y de Cholesky
 dfCovarianza = dfDiferencia.cov()
-matrizCovI = dfCovarianza.values
-cholesky = np.linalg.cholesky(dfCovarianza)
-cholesky = cholesky.transpose()
+cholesky = np.linalg.cholesky(dfCovarianza).transpose()
 #%% Simulo M veces la siguiente curva y generlo un array con todos los resultados
-arraySimulaciones = simulacionCurva(dfDiferencia)
+arraySimulaciones = simulacionCurva(dfDiferencia,25)
+#%% Grafico para ver nodos simulados
+Xline, smoothArray = smoothLine(arraySimulaciones)
+plt.plot(Xline,smoothArray)
+plt.xlim(30, 360)
+plt.xticks(np.arange(30, 361, 30))
+plt.ylabel("Tasa FTP")
+plt.xlabel("Tiempo (días)")
+plt.title("Simulación de Tasas de Interés")
+plt.show()
